4ac16220a5c2994e3fdba69072e3e28a6f806770
Lucas Gonçalves <lucas.goncalves@forpeople.io>
2023-10-27 17:47:16 -0300

    Add project files.

---
+++ MethodParameters/Product.cs
@@ -0,0 +1,42 @@
+﻿namespace MethodParameters;
+
+public class Product
+{
+    public int Balance { get; set; }
+    public string Name { get; set; }
+
+    public Product(int balance, string name)
+    {
+        Balance = balance;
+        Name = name;
+    }
+
+    public static void ChangeProductByReference(ref Product product)
+    {
+        product = new Product(balance: 99999, name: "changed");
+    }
+
+    public static void ChangeProductByValue(Product product)
+    {
+        product = new Product(balance: 99999, name: "changed");
+    }
+
+    public static void ChangeListProductByReference(ref List<Product> products)
+    {
+        foreach (Product product in products)
+        {
+            product.Balance = 99999;
+            product.Name = "changed";
+        }
+    }
+
+    public static void ClearListProductByValue(List<Product> products)
+    {
+        products.Clear();
+    }
+
+    public override string ToString()
+    {
+        return $"Balance: {Balance} | Name: {Name}";
+    }
+}
---
+++ MethodParameters/Program.cs
@@ -0,0 +1,28 @@
+﻿using MethodParameters;
+
+Product myProduct = new(balance: 0, name: "Lucas1");
+Product.ChangeProductByReference(ref myProduct);
+Console.WriteLine(myProduct); //Balance: 99999 | Name: changed
+
+myProduct = new(balance: 0, name: "Lucas2");
+Product.ChangeProductByValue(myProduct);
+Console.WriteLine(myProduct); //Balance: 0 | Name: Lucas2
+
+List<Product> products = new()
+{
+    new(balance: 0, name: "Lucas3"),
+    new(balance: 0, name: "Lucas4"),
+};
+Product.ChangeListProductByReference(ref products);
+products.ForEach(x => Console.WriteLine(x)); //ALL: Balance: 99999 | Name: changed
+
+products = new()
+{
+    new(balance: 0, name: "Lucas5"),
+    new(balance: 0, name: "Lucas6"),
+};
+Product.ClearListProductByValue(products);
+if (!products.Any())
+{
+    Console.WriteLine("Empty!"); //Empty!
+}; 
